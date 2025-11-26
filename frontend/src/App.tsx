6ff742847ad0ee5c1794033699b9083702782a1c
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-11-26 19:11:48 +0100

    Updated dist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,18 @@ import WishPage from "./pages/Wishes"
 export default function App() {
   return (
     <div>
+      <section className="flex flex-col items-center justify-center text-center py-20">
+        <h2 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text 
+                       bg-linear-to-r from-red-400 via-green-400 to-pink-400
+                       bg-size-[200%_200%] animate-gradient-colors text-glow
+                       leading-tight">
+          Welcome to Viktor's wishlist
+        </h2>
+        <p className="text-gray-400 max-w-xl mb-8">
+          This is my wishlist
+        </p>
+ 
+      </section>
       <WishPage/>
     </div>
     //<HashRouter >
