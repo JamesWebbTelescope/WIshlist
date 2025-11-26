4fa986d45f1f03440d59a9a5bf16f83ec1e41ae5
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-11-26 18:02:38 +0100

    Updated routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,18 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 import "./index.css"
 import Home from "./pages/Home";
 import WishPage from "./pages/Wishes"
+
 //import Products from "./pages/Products";
 //import Invoice from "./pages/Invoice";
 //import Admin from "./pages/Admin";
 
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
+    <HashRouter >
+      <Route>
         <Route path="/Wishlist" element={<Home />} />
         <Route path="/WIshlist/wishes" element= {<WishPage/>}/>
-      </Routes>
+      </Route>
     </HashRouter>
   );
 }
