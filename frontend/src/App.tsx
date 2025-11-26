abca00b8c1df4e3e3558cb346437234f6acbf673
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-11-26 19:27:58 +0100

    Updated dist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,17 +11,18 @@ export default function App() {
   return (
     <div>
       <section className="flex flex-col items-center justify-center text-center py-20">
-        <h2 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text 
-                       bg-linear-to-r from-red-400 via-green-400 to-pink-400
-                       bg-size-[200%_200%] animate-gradient-colors text-glow
-                       leading-tight">
-          Welcome to Viktor's wishlist
-        </h2>
-        <p className="text-gray-400 max-w-xl mb-8">
-          This is my wishlist
-        </p>
+                <h2 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text 
+                            bg-linear-to-r from-red-400 via-green-400 to-pink-400
+                            bg-size-[200%_200%] animate-gradient-colors text-glow
+                            leading-tight">
+                Welcome to Viktor's wishlist
+                </h2>
+                <p className="text-gray-400 max-w-xl mb-8">
+                This is my wishlist
+                </p>
  
-      </section>
+            </section>
+
       <WishPage/>
     </div>
     //<HashRouter >
