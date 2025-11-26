107cee24f7a8a06d2ac6a9a6b08ad8ee16a4157b
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-11-26 19:31:30 +0100

    Update Wishes.tsx

--- frontend/src/pages/Wishes.tsx
+++ frontend/src/pages/Wishes.tsx
@@ -2,12 +2,12 @@ import Layout from "../components/Layout";
 //import { useState, useEffect } from "react";
 //import { GetWishes} from "../services/apiServices";
 
-interface DisplayWishes {
+/*interface DisplayWishes {
     id: number;
     price: number;
     name: string;
     link: string;
-}
+}*/
 
 export default function WishPage() {
     /*const [wishes, setWishData] = useState<DisplayWishes[]>([]);
@@ -43,19 +43,6 @@ export default function WishPage() {
     return(
     <Layout>
         <div>
-            <section className="flex flex-col items-center justify-center text-center py-20">
-                <h2 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text 
-                            bg-linear-to-r from-red-400 via-green-400 to-pink-400
-                            bg-size-[200%_200%] animate-gradient-colors text-glow
-                            leading-tight">
-                Welcome to Viktor's wishlist
-                </h2>
-                <p className="text-gray-400 max-w-xl mb-8">
-                This is my wishlist
-                </p>
- 
-            </section>
-
 
                 <table className="table">
                 <thead>
