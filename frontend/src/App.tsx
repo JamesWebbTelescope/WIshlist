a94e55eebb722bd1976ea4ef446e98e76db5ed30
JamesWebbTelescope <Viktorfrom@gmail.com>
2025-11-29 17:08:17 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
 
       <WishPage/>
       <div>
-      <BrowserRouter basename="/Wishlist/frontend/src/pages/">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="Wishlist/frontend/src/pages/Wishes" element= {<WishPage/>}/>
